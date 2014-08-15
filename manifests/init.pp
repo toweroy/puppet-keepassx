81683247461d674aec5e6e41e0f1a0d5756502e1
Richard Tolman <rtolman@spotify.com>
2014-08-15 15:49:41 +0200

    Fix: Wrong class name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Public: Install KeePassX.app into /Applications.
 #
 # include keepassx
-class dropbox {
+class keepassx {
   package { 'KeePassX':
     provider => 'appdmg',
     source   => 'http://www.keepassx.org/releases/KeePassX-0.4.3.dmg'
